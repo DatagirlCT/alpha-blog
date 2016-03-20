4ee1ca991813f231edb9389fe36f3c1d003cd165
CloudQS <pauline@cloudqs.co.za>
2016-03-20 15:56:34 +0000

    Add associations between Article and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 105 },
               uniqueness: { case_sensitive: false },
               format: { with: VALID_EMAIL_REGEX }
+              
+  has_many :articles
+  
+  before_save { self.email = email.downcase }
+  
 end
